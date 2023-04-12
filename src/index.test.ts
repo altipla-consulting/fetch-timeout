3e627e25255d4cf10a8122ec1bb39aa0c468a1cc
Ernesto Alejo <ernestokarim@gmail.com>
2023-04-12 18:14:16 +0200

    Implement a fetch with timeout over the underlying node-fetch or standard fetch functions.

---
+++ src/index.test.ts
@@ -0,0 +1,43 @@
+
+import { fetchTimeout } from './index'
+import { test, expect } from 'vitest'
+import { AbortError, Response as NodeFetchResponse } from 'node-fetch'
+
+
+//@ts-expect-error Node 16 does not have global support for responses yet.
+globalThis.Response = NodeFetchResponse
+
+function mockFetch(ms: number) {
+  globalThis.fetch = function(_request: URL | RequestInfo, options?: RequestInit) {
+    return new Promise((resolve, reject) => {
+      options?.signal?.addEventListener('abort', () => reject(new AbortError('The operation was aborted.')))
+      setTimeout(() => resolve(new Response('foo')), ms)
+    })
+  }
+}
+
+test('normal fetch within the timeout', async () => {
+  mockFetch(100)
+
+  let response = await fetchTimeout('https://www.example.com/', { timeout: 300 })
+  expect(await response.text()).toBe('foo')
+})
+
+test('fetch with timeout', async () => {
+  mockFetch(300)
+
+  expect(fetchTimeout('https://www.example.com/', { timeout: 100 })).rejects.toThrowError('The operation timed out.')
+})
+
+test('fetch aborted before timeout', async () => {
+  mockFetch(300)
+
+  let ctrl = new AbortController()
+  let response = fetchTimeout('https://www.example.com/', {
+    timeout: 200,
+    signal: ctrl.signal,
+  })
+  ctrl.abort()
+
+  expect(response).rejects.toThrowError('The operation was aborted.')
+})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
 
+import nodeFetch, { AbortError } from 'node-fetch'
+
+export async function fetchTimeout(request: RequestInfo, options?: RequestInit & { timeout?: number }): Promise<Response> {
+  let ctrl = new AbortController()
+  let ctrlTimeout: AbortController | undefined
+  try {
+    options?.signal?.addEventListener('abort', () => ctrl.abort())
+
+    let response = (globalThis.fetch || nodeFetch)(request, {
+      ...options,
+      signal: ctrl.signal,
+    })
+
+    if (options?.timeout) {
+      ctrlTimeout = new AbortController()
+      let aborter = abortableTimeout(options.timeout, ctrlTimeout.signal)
+      let race = await Promise.race([aborter, response])
+      if (!race) {
+        ctrl.abort()
+        throw new AbortError('The operation timed out.')
+      }
+    }
+
+    return response
+  } finally {
+    ctrlTimeout?.abort()
+  }
+}
+
+function abortableTimeout(ms: number, signal: AbortSignal): Promise<void> {
+  return new Promise((resolve, reject) => {
+    let id = setTimeout(resolve, ms)
+    signal.addEventListener('abort', () => {
+      clearTimeout(id)
+      reject()
+    })
+  })
+}
+
